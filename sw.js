3cc0f9b734c76fe77d54ddc3a4a3b7d2c4740d23
gaborvokcs2 <wagnergaborwagner503@gmail.com>
2026-06-29 18:56:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Palánta – Service Worker v11
-const CACHE = 'palanta-v11';
+// Palánta – Service Worker v12
+const CACHE = 'palanta-v12';
 
 // Telepítéskor azonnal átveszi az irányítást
 self.addEventListener('install', () => self.skipWaiting());
